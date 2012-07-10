77b6eabe615c902750a7e1d5ade912d94117a181
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-10 22:14:00 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# lanyrd-scraper
+
+Scraper for Lanyrd events
+
+## Getting Started
+Install the module with: `npm install https://github.com/markdalgleish/node-lanyrd-scraper/tarball/master`
+
+```javascript
+var lanyrd = require('lanyrd-scraper');
+lanyrd.scrape('2012/web-directions-code', function(err, data){
+	console.log(data); // Object containing event info.
+});
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Mark Dalgleish  
+Licensed under the MIT license.
---
+++ test/lanyrd-scraper_test.js
@@ -0,0 +1,140 @@
+var lanyrd = require('../lib/lanyrd-scraper.js'),
+	express = require('express');
+
+/*
+	======== A Handy Little Nodeunit Reference ========
+	https://github.com/caolan/nodeunit
+
+	Test methods:
+	test.expect(numAssertions)
+	test.done()
+	Test assertions:
+	test.ok(value, [message])
+	test.equal(actual, expected, [message])
+	test.notEqual(actual, expected, [message])
+	test.deepEqual(actual, expected, [message])
+	test.notDeepEqual(actual, expected, [message])
+	test.strictEqual(actual, expected, [message])
+	test.notStrictEqual(actual, expected, [message])
+	test.throws(block, [error], [message])
+	test.doesNotThrow(block, [error], [message])
+	test.ifError(value)
+*/
+
+var app = express.createServer();
+var PORT = 8535;
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express['static'](__dirname + '/mock-pages'));
+});
+
+app.listen(PORT);
+
+var multiDayEventData;
+exports['multi-day event'] = {
+	setUp: function(done) {
+		if (multiDayEventData) {
+			done();
+			return;
+		}
+
+		lanyrd.scrape('http://localhost:' + PORT + '/multi-day-event.html', function(err, res){
+			multiDayEventData = res;
+			done();
+		});
+	},
+	'has title': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.title, 'Web Directions Code');
+		test.done();
+	},
+	'has tagline': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.tagline, 'Melbourne, get your code on.');
+		test.done();
+	},
+	'has website': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.websiteUrl, 'http://code12melb.webdirections.org/');
+		test.done();
+	},
+	'has schedule': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.scheduleUrl, 'http://lanyrd.com/2012/web-directions-code/schedule/');
+		test.done();
+	},
+	'has location': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.location, 'Melbourne, Australia');
+		test.done();
+	},
+	'has venue': function(test) {
+		test.expect(3);
+		test.equal(multiDayEventData.venue.name, 'The RACV City Club');
+		test.equal(multiDayEventData.venue.address, '501 Bourke St, Melbourne, 3000');
+		test.equal(multiDayEventData.venue.googleMapsUrl, 'http://maps.google.com/maps?q=-37.8154734594,144.959473427+(The RACV City Club)');
+		test.done();
+	},
+	'has start date': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.startDate, 'May 23, 2012');
+		test.done();
+	},
+	'has end date': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.endDate, 'May 24, 2012');
+		test.done();
+	},
+	'has hashtag': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.hashtag, '#wdc12');
+		test.done();
+	},
+	'has Twitter account': function(test) {
+		test.expect(1);
+		test.equal(multiDayEventData.twitterHandle, '@webdirections');
+		test.done();
+	},
+	'has speakers': function(test) {
+		test.expect(4);
+		test.equal(multiDayEventData.speakers.length, 19);
+		test.equal(multiDayEventData.speakers[15].name, 'Mark Dalgleish');
+		test.equal(multiDayEventData.speakers[15].twitterHandle, '@markdalgleish');
+		test.equal(multiDayEventData.speakers[11].twitterHandle, undefined, 'supports speakers without Twitter handles');
+		test.done();
+	},
+	'has sessions': function(test) {
+		test.expect(5);
+		test.equal(multiDayEventData.sessions.length, 20);
+		test.equal(multiDayEventData.sessions[14].title, 'JavaScript: Getting Closure');
+		test.equal(multiDayEventData.sessions[14].speakerName, 'Mark Dalgleish');
+		test.equal(multiDayEventData.sessions[14].speakerTwitterHandle, '@markdalgleish');
+		test.equal(multiDayEventData.sessions[18].speakerTwitterHandle, undefined, 'supports speakers without Twitter handles');
+		test.done();
+	},
+	'has attendees': function(test) {
+		test.expect(3);
+		test.equal(multiDayEventData.attendees.length, 27);
+		test.equal(multiDayEventData.attendees[15].name, 'Mark Dalgleish');
+		test.equal(multiDayEventData.attendees[15].twitterHandle, '@markdalgleish');
+		test.done();
+	}
+};
+
+var EVENT_URL = 'http://lanyrd.com/2012/web-directions-code';
+exports['resolveUrl'] = {
+	'ignores complete url': function(test) {
+		test.expect(1);
+		test.equal(lanyrd.resolveUrl(EVENT_URL), EVENT_URL);
+		test.done();
+	},
+	'resolves urls correctly': function(test) {
+		test.expect(2);
+		test.equal(lanyrd.resolveUrl('2012/web-directions-code'), EVENT_URL);
+		test.equal(lanyrd.resolveUrl('/2012/web-directions-code'), EVENT_URL);
+		test.done();
+	}
+}
