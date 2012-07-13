b26d002013f2c8360980cf98bad9258084ededb5
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-13 10:48:56 +1000

    Add tests for helper methods

--- test/helper_test.js
+++ test/helper_test.js
@@ -1,4 +1,5 @@
-var Helper = require('../lib/helper');
+var Helper = require('../lib/helper'),
+	$ = require('jquery');
 
 /*
 	======== A Handy Little Nodeunit Reference ========
@@ -20,7 +21,8 @@ var Helper = require('../lib/helper');
 	test.ifError(value)
 */
 
-var helper = new Helper();
+var helper = new Helper($);
+
 var EVENT_URL = 'http://lanyrd.com/2012/web-directions-code';
 exports['resolveUrl'] = {
 	'ignores complete url': function(test) {
@@ -32,6 +34,72 @@ exports['resolveUrl'] = {
 		test.expect(2);
 		test.equal(helper.resolveUrl('2012/web-directions-code'), EVENT_URL);
 		test.equal(helper.resolveUrl('/2012/web-directions-code'), EVENT_URL);
+		test.done();
+	}
+};
+
+exports['setContext'] = {
+	'sets the context': function(test)	{
+		test.expect(1);		
+		helper.setContext($);
+		test.equal(helper.$, $);
+		test.done();
+	}
+};
+
+var TRIMMED_TEXT = 'Trimmed text',
+	$getTextContent = $('<div><div id="blank-text">   </div><div id="text"> ' + TRIMMED_TEXT + ' </div></div>');
+exports['getText'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(2);
+		test.equal(helper.getText($getTextContent.find('#nonexistant-text')), undefined);
+		test.equal(helper.getText($getTextContent.find('#blank-text')), undefined);
+		test.done();
+	},
+	'returns the text': function(test) {
+		test.expect(1);
+		test.equal(helper.getText($getTextContent.find('#text')), TRIMMED_TEXT);
+		test.done();
+	}
+};
+
+var DOMAIN = 'http://lanyrd.com',
+	PATH = '/test/url',
+	$getHrefContent = $('<div><a id="link" href="' + PATH + '">Test Link</a></div>');
+exports['getHref'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(1);
+		test.equal(helper.getHref($getHrefContent.find('#nonexistant-link')), undefined);
+		test.done();
+	},
+	'returns the full URL': function(test) {
+		test.expect(1);
+		test.equal(helper.getHref($getHrefContent.find('#link')), DOMAIN + PATH);
+		test.done();
+	}
+};
+
+var TITLE = ' Trimmed title',
+	$getTitleContent = $('<div><div id="title" title="' + TITLE + '" /></div>');
+exports['getTitle'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(1);
+		test.equal(helper.getTitle($getTitleContent.find('#nonexistant-title')), undefined);
+		test.done();
+	},
+	'returns the title': function(test) {
+		test.expect(1);
+		test.equal(helper.getTitle($getTitleContent.find('#title')), TITLE);
+		test.done();
+	}
+};
+
+var TWITTER_HANDLE = '@markdalgleish',
+	$getTwitterHandleContent = $('<div><a id="twitter-handle" href="/profile/' + TWITTER_HANDLE.replace('@','') + '" /></div>');
+exports['getTwitterHandle'] = {
+	'returns the Twitter handle': function(test) {
+		test.expect(1);
+		test.equal(helper.getTwitterHandle($getTwitterHandleContent.find('#twitter-handle')), TWITTER_HANDLE);
 		test.done();
 	}
 };
