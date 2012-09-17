74bfc9864bcf8f1742f34d1d44d32eb13cfb829a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-09-17 23:10:25 +1000

    Refactor Helper into Page and URL Resolver

---
+++ test/page_test.js
@@ -0,0 +1,73 @@
+var Page = require('../lib/page'),
+	cheerio = require('cheerio');
+
+var TRIMMED_TEXT = 'Trimmed text',
+	getTextpage = new Page('<div><div id="blank-text">   </div><div id="text"> ' + TRIMMED_TEXT + ' </div></div>');
+exports['getText'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(1);
+		test.equal(getTextpage.getText('#nonexistant-text'), undefined);
+		test.done();
+	},
+	'returns undefined if blank': function(test) {
+		test.expect(1);
+		test.equal(getTextpage.getText('#blank-text'), undefined);
+		test.done();
+	},
+	'returns the text': function(test) {
+		test.expect(1);
+		test.equal(getTextpage.getText('#text'), TRIMMED_TEXT);
+		test.done();
+	}
+};
+
+var DOMAIN = 'http://lanyrd.com',
+	PATH = '/test/url',
+	getHrefpage = new Page('<div><a id="link" href="' + PATH + '">Test Link</a></div>');
+exports['getHref'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(1);
+		test.equal(getHrefpage.getHref('#nonexistant-link'), undefined);
+		test.done();
+	},
+	'returns the full URL': function(test) {
+		test.expect(1);
+		test.equal(getHrefpage.getHref('#link'), DOMAIN + PATH);
+		test.done();
+	}
+};
+
+var TITLE = ' Trimmed title',
+	getTitlepage = new Page('<div><div id="title" title="' + TITLE + '" /></div>');
+exports['getTitle'] = {
+	'returns undefined if not found': function(test) {
+		test.expect(1);
+		test.equal(getTitlepage.getTitle('#nonexistant-title'), undefined);
+		test.done();
+	},
+	'returns the title': function(test) {
+		test.expect(1);
+		test.equal(getTitlepage.getTitle('#title'), TITLE);
+		test.done();
+	}
+};
+
+var TWITTER_HANDLE = 'markdalgleish',
+	getTwitterHandlepage = new Page('<div><a id="twitter-handle" href="/profile/' + TWITTER_HANDLE + '" /></div>');
+exports['getTwitterHandle'] = {
+	'returns the Twitter handle': function(test) {
+		test.expect(1);
+		test.equal(getTwitterHandlepage.getTwitterHandle('#twitter-handle'), TWITTER_HANDLE);
+		test.done();
+	}
+};
+
+var HASHTAG = 'melbjs',
+	getHashTagpage = new Page('<div><div id="hashtag">' + HASHTAG + '</div></div>');
+exports['getHashTag'] = {
+	'returns the hash tag': function(test) {
+		test.expect(1);
+		test.equal(getHashTagpage.getHashtag('#hashtag'), HASHTAG);
+		test.done();
+	}
+};
---
+++ test/urlresolver_test.js
@@ -0,0 +1,34 @@
+var resolver = require('../lib/urlresolver');
+
+exports['resolveUrl'] = {
+	'converts shorthand url without a leading slash into full version': function(test) {
+		test.equals(resolver.resolveUrl('series/melbjs'), 'http://lanyrd.com/series/melbjs');
+		test.done();
+	},
+	'converts shorthand url with a leading slash into full version': function(test) {
+		test.equals(resolver.resolveUrl('/series/melbjs'), 'http://lanyrd.com/series/melbjs');
+		test.done();
+	}
+};
+
+exports['shortenUrl'] = {
+	'shortens full url with www': function(test) {
+		test.equals(resolver.shortenUrl('http://www.lanyrd.com/series/melbjs'), 'series/melbjs');
+		test.done();
+	},
+	'shortens full url without www': function(test) {
+		test.equals(resolver.shortenUrl('http://lanyrd.com/series/melbjs'), 'series/melbjs');
+		test.done();
+	},
+	'removes leading slash from short url': function(test) {
+		test.equals(resolver.shortenUrl('/series/melbjs'), 'series/melbjs');
+		test.done();
+	}
+};
+
+exports['resolvePageType'] = {
+	'returns the correct page type': function(test) {
+		test.equals(resolver.resolvePageType('/foobar', { foo: /foo/ } ), 'foo');
+		test.done();
+	}
+};
