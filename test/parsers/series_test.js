a0c624d2d6172d31aa3c1f54994145108ff4688d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-09-18 21:35:32 +1000

    Add date, time and location to series data

--- test/parsers/series_test.js
+++ test/parsers/series_test.js
@@ -23,10 +23,13 @@ exports['series'] = {
 		test.done();
 	},
 	'has events': function(test) {
-		test.expect(3);
+		test.expect(6);
 		test.equal(seriesData.events.length, 5);
 		test.equal(seriesData.events[0].title, 'MelbJS');
 		test.equal(seriesData.events[0].url, 'http://lanyrd.com/2012/melbjs-november/');
+		test.equal(seriesData.events[0].date, '14th November 2012');
+		test.equal(seriesData.events[0].time, '6pm');
+		test.equal(seriesData.events[0].location, 'Australia, Melbourne');
 		test.done();
 	}
 };
